374466e640d8497feada90df05728bcd45938b85
claylolx <145122199+claylolx@users.noreply.github.com>
2024-12-17 17:18:45 +0100

    FIGMA - v1.0

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById('toggle-theme').addEventListener('click', () => {
+// Referencia al boton de cambio de tema
+const toggleThemeBtn = document.getElementById('toggle-theme');
+
+// Comprobar y aplicar la preferencia guardada al cargar la página
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+}
+
+// Evento para cambiar el tema
+toggleThemeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
+
+    // Guardar el estado actual en localStorage
+    if (document.body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 });
+
+
